4b3d5584c8fdb9fa1d147f108d1911dfca0742ef
Majok-Bol <bolmajok001@gmail.com>
2024-07-02 19:05:41 +0300

    added list items when add button is clicked

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 console.log("Hello World");
 //select body element
 const body = document.querySelector("body");
+//select input element
+const input = document.getElementById("txt");
+
 function add() {
   const myButton = document.getElementById("btn1");
   myButton.addEventListener("click", () => {
@@ -14,14 +17,25 @@ function add() {
     } else {
       document.getElementById("paragraph").textContent = inputText.value;
     }
+    //create list
+    const listItems = document.createElement("li");
+    listItems.textContent = value;
+    // append it to the unordered list
+    document.getElementById("myList").appendChild(listItems);
+    //clear input field
+    inputText.value = "";
   });
 }
-add();
 
 function removeFunction() {
   const button2 = document.getElementById("btn2");
   button2.addEventListener("click", () => {
-    document.getElementById("paragraph").textContent = "";
+    document.getElementById("myList");
+    //check if myList lastchild
+    if (myList.lastChild) {
+      myList.removeChild(myList.lastChild);
+    }
   });
 }
+add();
 removeFunction();
