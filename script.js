bdd38d6c5aece89157815c74fd77a3aec1de46c0
Majok-Bol <bolmajok001@gmail.com>
2024-07-02 12:15:37 +0300

    added functionality on add button

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Hello World")
+console.log("Hello World");
+// select input element
+let input = document.getElementById("txt");
+// add event listener
+// show focus area
+input.addEventListener("focus", () => {
+  input.style.backgroundColor = "aqua";
+});
+// if one selects add button then add inputs into the paragraph
+const myButton = document.getElementById("btn1");
+myButton.addEventListener("click", function (e) {
+  // get input element value
+  let inputText = document.getElementById("txt");
+  document.getElementById("paragraph").textContent = inputText.value;
+});
