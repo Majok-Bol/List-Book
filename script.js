82974c492b232d5056f5e3f3ab0e802dc6831dbc
Majok-Bol <bolmajok001@gmail.com>
2024-07-02 18:23:47 +0300

    added error check for input value

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
+"use strict";
 console.log("Hello World");
-// select input element
-let input = document.getElementById("txt");
-// add event listener
-// show focus area
-input.addEventListener("focus", () => {
-  input.style.backgroundColor = "aqua";
-});
-// if one selects add button then add inputs into the paragraph
-const myButton = document.getElementById("btn1");
-myButton.addEventListener("click", function (e) {
-  // get input element value
-  let inputText = document.getElementById("txt");
-  document.getElementById("paragraph").textContent = inputText.value;
-});
+//select body element
+const body = document.querySelector("body");
+function add() {
+  const myButton = document.getElementById("btn1");
+  myButton.addEventListener("click", () => {
+    // get input element value
+    let inputText = document.getElementById("txt");
+    const value = inputText.value;
+    if (value.trim() === "") {
+      document.getElementById("paragraph").textContent =
+        "Please enter valid input";
+    } else {
+      document.getElementById("paragraph").textContent = inputText.value;
+    }
+  });
+}
+add();
+
+function removeFunction() {
+  const button2 = document.getElementById("btn2");
+  button2.addEventListener("click", () => {
+    document.getElementById("paragraph").textContent = "";
+  });
+}
+removeFunction();
